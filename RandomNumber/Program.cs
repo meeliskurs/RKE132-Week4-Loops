ead62e33632ccd31d5f3fb5922ed0783f265c78d
Meelis Kurs <meelis.kurs@tktk.ee>
2022-10-28 23:52:52 +0300

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+     myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
+
+
+
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//for-loop
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don´t need no education.");
+}
+
+Console.WriteLine("Have a nice day.");
+
